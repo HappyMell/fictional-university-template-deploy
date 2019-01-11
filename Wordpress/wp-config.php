2fa61c540276213a92d8b138d0a563da23adec4f
HappyMell <mellisamadrid@hotmail.com>
2019-01-11 21:25:58 +0000

    debug

--- Wordpress/wp-config.php
+++ Wordpress/wp-config.php
@@ -82,9 +82,16 @@ $table_prefix  = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define('WP_DEBUG', true);
-define('WP_DEBUG_LOG', true);
-define( 'WP_DEBUG_DISPLAY', true );
+ // Enable WP_DEBUG mode
+define( 'WP_DEBUG', true );
+
+// Enable Debug logging to the /wp-content/debug.log file
+define( 'WP_DEBUG_LOG', true );
+
+// Disable display of errors and warnings 
+define( 'WP_DEBUG_DISPLAY', false );
+@ini_set( 'display_errors', 0 );
+
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
